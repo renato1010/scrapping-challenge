ef36b3c43ed8ef8fc0e0772069bff447c96ef826
Richard Bliss <rbliss@users.noreply.github.com>
2023-03-17 18:24:55 -0600

    Update copy

--- packages/frontend/src/pages/Root.tsx
+++ packages/frontend/src/pages/Root.tsx
@@ -4,8 +4,8 @@ import { Layout } from '../Layout';
 export default function Root() {
   return (
     <Layout header='Hello'>
-      <p>A challenge awaits...</p>
       <p>Please check the README.md on the project for details.</p>
+      <p>A challenge awaits...</p>
     </Layout>
   );
 }
