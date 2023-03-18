577bd801eeff9d1b8bf080e59595b64ed54fd00e
Richard Bliss <rbliss@users.noreply.github.com>
2023-03-17 18:23:39 -0600

    Add note about challenge location

--- packages/frontend/src/pages/Root.tsx
+++ packages/frontend/src/pages/Root.tsx
@@ -5,6 +5,7 @@ export default function Root() {
   return (
     <Layout header='Hello'>
       <p>A challenge awaits...</p>
+      <p>Please check the README.md on the project for details.</p>
     </Layout>
   );
 }
